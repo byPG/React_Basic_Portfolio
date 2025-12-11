d534e7f061b2ffa8f3e530e3b90d579d7f5cd8b1
Paulina <pg.gastol@gmail.com>
2025-12-11 20:26:25 +0100

    basic functions of toDo list

--- vite-react-toDo/src/App.jsx
+++ vite-react-toDo/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [textTask, setTextTask] = useState(""); // tekst który wpisujesz do inputa - zadanie
+  const [tasks, setTasks] = useState([]); // dodawanie nowych tasków do listy
+
+  function handleChange(event) {
+    setTextTask(event.target.value);
+  }
+
+  function addTask() {
+    if (textTask.trim() === "") return; // walidcja, trim ucina biale znaki z przodu i z tylu, potem sprawdzamy czy pole jest puste
+    setTasks([...tasks, textTask]);
+    setTextTask(""); // wyczyszczenie inputa
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <label>Task: </label>
+      <input type="text" value={textTask} onChange={handleChange} />
+      <button onClick={addTask}>Dodaj</button>
+
+      <ol>
+        {tasks.map(
+          (
+            task,
+            index // modyfikacja elementow tablicy do li
+          ) => (
+            <li key={index}>{task}</li>
+          )
+        )}
+      </ol>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
