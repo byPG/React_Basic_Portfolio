79b742265dd85a006a8b308034e130bc6af99ab2
Paulina <pg.gastol@gmail.com>
2025-12-12 10:55:28 +0100

    changing structure

--- vite-react-toDo/src/App.jsx
+++ vite-react-toDo/src/App.jsx
@@ -1,49 +1,11 @@
-import { useState } from "react";
 import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
 
 function App() {
-  const [textTask, setTextTask] = useState(""); // tekst który wpisujesz do inputa - zadanie
-  const [tasks, setTasks] = useState([]); // dodawanie nowych tasków do listy
-
-  function handleChange(event) {
-    setTextTask(event.target.value);
-  }
-
-  function addTask() {
-    if (textTask.trim() === "") return; // walidcja, trim ucina biale znaki z przodu i z tylu, potem sprawdzamy czy pole jest puste
-    setTasks([...tasks, textTask]);
-    setTextTask(""); // wyczyszczenie inputa
-  }
-
   return (
     <>
       <Header></Header>
-      <main>
-        <div className="flex justify-between ">
-          <input
-            type="text"
-            value={textTask}
-            onChange={handleChange}
-            placeholder="Write your task here..."
-          />
-          <button
-            onClick={addTask}
-            className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
-            Dodaj
-          </button>
-        </div>
-
-        <ol>
-          {tasks.map(
-            (
-              task,
-              index // modyfikacja elementow tablicy do li
-            ) => (
-              <li key={index}>{task}</li>
-            )
-          )}
-        </ol>
-      </main>
+      <Main></Main>
     </>
   );
 }
---
+++ vite-react-toDo/src/components/Main.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function Main() {
+  const [textTask, setTextTask] = useState(""); // tekst który wpisujesz do inputa - zadanie
+  const [tasks, setTasks] = useState([]); // dodawanie nowych tasków do listy
+
+  function handleChange(event) {
+    setTextTask(event.target.value);
+  }
+
+  function addTask() {
+    if (textTask.trim() === "") return; // walidcja, trim ucina biale znaki z przodu i z tylu, potem sprawdzamy czy pole jest puste
+    setTasks([...tasks, textTask]);
+    setTextTask(""); // wyczyszczenie inputa
+  }
+
+  return (
+    <main>
+      <div className="flex justify-between ">
+        <input
+          type="text"
+          value={textTask}
+          onChange={handleChange}
+          placeholder="Write your task here..."
+        />
+        <button
+          onClick={addTask}
+          className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
+          Dodaj
+        </button>
+      </div>
+
+      <ol>
+        {tasks.map(
+          (
+            task,
+            index // modyfikacja elementow tablicy do li
+          ) => (
+            <li key={index}>{task}</li>
+          )
+        )}
+      </ol>
+    </main>
+  );
+}
